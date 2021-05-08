eca6bc8fee85019370de447d3db7675e58ce8ef2
Harry Ghazni <harry.ghazni@thrivelearning.com>
2021-05-08 14:48:59 +0100

    Removes flow key from object

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -36,7 +36,7 @@ module.exports = {
       test: /\.(ts|tsx)$/,
       loader: require.resolve("babel-loader"),
       options: {
-        presets: [["react-app", { flow: false, typescript: true }]]
+        presets: [["react-app", { typescript: true }]]
       }
     });
     config.resolve.extensions.push(".ts", ".tsx");
